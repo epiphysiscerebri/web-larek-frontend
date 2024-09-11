edeb630e905abc516b196668916247e40a7cf989
Anton Zamuriev <zamurievanton@inbox.ru>
2024-09-11 13:14:42 +0300

    Создал модель данных

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
-export type PaymentMethod = "online" | "offline"
+export type PaymentMethod = "card" | "cash"
 
-export interface IProductCard {
+// Описание единицы продукта
+export interface IProduct {
   id: string;
   description: string;
   image: string;  
@@ -9,40 +10,135 @@ export interface IProductCard {
   price: number | null;
 }
 
-export interface IBasket {
-  items: IProductCard[];
-  total: number;
+// Объект настроек для продукта, который бьудет использоваться во view
+export interface IProductSettings {
+  text: string;
+  image: string;  
+  title: string;
+  category: string;
+  price: number | null;
+  compactClass: string;
+  isCompact: boolean;
+} 
+
+// Описание корзины
+// export interface IBasket {
+//   items: IProduct[];
+// }
+
+// Объект настроек для корзины, который бьудет использоваться во view
+export interface IBasketSettings {
+  listItem: string;
+  price: string;
 }
 
-export interface IFormOrder {
-  payment: PaymentMethod;
-  email: string;
-  phone: string;
-  address: string;
+// Описание заказа
+export interface IOrder extends IContacts, IPayment {
   total: number;
   items: string[];
 }
 
-export interface IFormOrderResult {
+// Объект настроек для заказа, который бьудет использоваться во view
+export interface IOrderSettings {
+  card?: string;
+  cash?: string;
+  address: string;
+  email: string;
+  phone: string;
+}
+
+// Описание результата заказа, который на присылает сервер, когда мы совершаем заказ 
+export interface IOrderResult {
   id: string;
   total: number;
 }
 
-export interface IModel {
-  cardsList?: IProductCard[];
-  product?: IProductCard;
-  basket?: IBasket;
-  order?: IFormOrderResult;
-  stateModal: Boolean;
+// Объект настроек для результата заказа, который бьудет использоваться во view
+export interface IOrderResultSettings {
+  description: string;
 }
 
-export interface IController {
-  model: IModel;
+// Описание АПИ
+export interface IWebLarekApi {
+  getProduct: (id: string) => Promise<IProduct>;
+  getProductList: () => Promise<IProduct[]>;
+  postOrder: (order: IOrder) => Promise<IOrderResult>;
+}
+
+// Описание Модели данных
+export interface AppState {
+  // Загружаемые с сервера данные
+  products?: Map<string, IProduct>;
+  selectedProduct?: IProduct | null;
+
+  // Заполняемые пользователем данные
+  openedModal: AppStateModals | null;
+  basket:  Map<string, IProduct>;
+  basketTotal: number;
+  payment: IPayment | null;
+  contacts: IContacts | null;
+  order: IOrder | null;
+
+  // Состояние интерфейса
+  isOrderReady: boolean;
+  modalMessage: string | null;
+  isError: boolean;
+
+  // Действия с API
+  loadProducts(): Promise<void>;
+  loadProduct(id: string): Promise<void>;
+  orderProducts(): Promise<IOrderResult>;
+
+  // Методы для работы с модальными окнами
+  openModal(modal: AppStateModals): void;
+	setMessage(message: string | null, isError: boolean): void;
+
+  // Пользовательские действия
+  fillContacts(contacts: Partial<IContacts>): void;
+  fillPayment(payment: Partial<IPayment>): void;
+  isValidContacts(): boolean;
+  isValidPayment(): boolean;
+}
+
+// Описание контактов
+export interface IContacts {
+  email: string;
+  phone: string;
+}
+
+// Описание оплаты
+export interface IPayment {
+  payment: PaymentMethod;
+  address: string;
+}
+
+// Описание событий для модальных окон
+export enum AppStateModals {
+  product = 'modal:product',
+  basket = 'modal:basket', 
+  payment = 'modal:payment',
+  contacts = 'modal:contacts',
+  success = 'modal:success',
+	none = 'modal:none',
+}
+
+// Описание событий для изменений Модели
+export enum AppStateChanges {
+  products = 'change:product',
+	modal = 'change:modal',
+	modalMessage = 'change:modalMessage',
+	selectedProduct = 'change:selectedProduct',
+	basket = 'change:basket',
+	order = 'change:order',
+}
+
+// Описание настройки Модели
+export interface AppStateSettings {
+  currency: string;
+  storageKey: string;
+  // Эта функция будет вызываться при изменении Модели
+  onChange: (changed: AppStateChanges) => void;
 }
 
 
-export interface IView {
-  mount: (arr: HTMLElement[]) => void;
-  controller: IController;
-}
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,56 @@ export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 
 export const settings = {
+  pageSelector: ".page",
+  pageSettings: {
+    wrapper: ".page__wrapper",
+  },
 
+  gallerySelector: ".gallery",
+  gallerySettings: {
+    itemClass: "gallery__item"
+  },
+
+  cardSelector: ".card_full",
+  cardCatalogTemplate: "#card-catalog",
+  cardPreviewTemplate: "#card-preview",
+  cardBasketTemplate: "#card-basket",
+  cardSettings: {
+    text: ".card__text",
+    image: ".card__image",  
+    title: ".card__title",
+    category: ".card__category",
+    price: ".card__price",
+    compactClass: ".card_compact",
+  },
+
+  basketSelector: ".basket",
+  basketTemplate: "#basket",
+  basketSettings: {
+    listItem: ".basket__list",
+    price: ".basket__price"
+  },
+  basketHeaderSelector: ".header__basket",
+  basketHeaderSettings: {
+    counter: ".header__basket-counter"
+  },
+
+  orderSelector: ".order",
+  orderTemplate: "#order",
+  orderSettings: {
+    orderCard: "#card",
+    orderCash: "#cash",
+    address: "#address",
+  },
+  orderContactsTemplate: "#contacts",
+  orderContactsSettings: {
+    email: "#email",
+    phone: "#phone",
+  },
+
+  successSelector: ".order-success",
+  successTemplat: "#success",
+  successSettings: {
+    description: ".order-success__description"
+  }
 };
